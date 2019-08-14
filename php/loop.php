2704ae5a8bd05adbe1f0c5d0fe90568162aab81d
HilmiSatriaji21 <hilmisatriaji21@gmail.com>
2019-08-14 15:27:09 +0700

    acece

--- php/loop.php
+++ php/loop.php
@@ -1,5 +1,22 @@
 <?php
 $loop =  11;
-for ($i=1; $i < $loop; $i++) { 
-    echo "Aku Data Ke- " . $i . "<br>";
+for ($i = 0; $i < $loop; $i++) { 
+    if ($i < 1) {
+        echo "Aku Data Ke- " . $i ." Dan Mendapatkan Mendali Emas<br>";
+    }
+    elseif ($i < 2) {
+        echo "Aku Data Ke- " . $i ." Dan Mendapatkan Mendali Perak<br>";
+    }
+    elseif ($i < 3) {
+        echo "Aku Data Ke- " . $i ." Dan Mendapatkan Mendali Perunggu<br>";
+    }
+    elseif ($i < 7) {
+        echo "Aku Data Ke- " . $i ." Dan Mendapatkan Tas Laptop<br>";  
+    }
+    elseif ($i < 10) {
+        echo "Aku Data Ke- " . $i ." Dan Mendapatkan Tas Karung<br>";  
+    }
+    elseif ($i < 11) {
+        echo "Aku Data Ke- " . $i ." Dan Tidak Mendapatkan Apapun<br>";  
+    }
 }
