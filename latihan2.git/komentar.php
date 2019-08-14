4aca91259a18dbcfa4c129990de3d5f81e12affa
HilmiSatriaji21 <hilmisatriaji21@gmail.com>
2019-08-14 14:12:38 +0700

    inisial realese

---
+++ latihan2.git/komentar.php
@@ -0,0 +1,10 @@
+<?php
+/* Ini Komentar Tidak Akan Tercetak Di Layar
+   Yang Ini Juga Komentar
+*/
+
+// Nah Kalo Ini Komentar Satu Baris
+
+# Terus Kalo Ini Juga Komentar
+
+echo "Ini Akan Tercetak Di Layar";
---
+++ latihan2.git/konstanta.php
@@ -0,0 +1,7 @@
+<?php
+define ("Nama", "Hilmi Satriaji");
+define ("Nilai", 98);
+
+//Nama = "Alice"; //Akan Menyebabkan Error
+echo "Nama : " . Nama;
+echo "<br> Nilai : " . Nilai;
